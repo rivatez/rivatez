3f3f019bebe78b3ae978226c1383fbe941cd8652
rivatez <31881226+rivatez@users.noreply.github.com>
2018-03-03 12:06:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -16,59 +16,7 @@ function update() {
         document.body.removeChild(document.querySelector("#coverup"));
         playing = true;
     }
-    
-    if (music.currentTime / music.duration >= 0.99) {
-        music.currentTime = 0.01;
-        music.play();
-    }
-    
-    if (music.currentTime / music.duration > 0.05 && music.currentTime / music.duration < 0.08) {
-        output.innerText = "BEWARE";
-    }
-    if (music.currentTime / music.duration > 0.08 && music.currentTime / music.duration < 0.14) {
-        output.innerText = "THE";
-    }
-    if (music.currentTime / music.duration > 0.14 && music.currentTime / music.duration < 0.18) {
-        output.innerText = "LOLLIPOP";
-    }
-    if (music.currentTime / music.duration > 0.18 && music.currentTime / music.duration < 0.21) {
-        output.innerText = "";
-    }
-    if (music.currentTime / music.duration > 0.21 && music.currentTime / music.duration < 0.24) {
-        output.innerText = "OF";
-    }
-    if (music.currentTime / music.duration > 0.24 && music.currentTime / music.duration < 0.33) {
-        output.innerText = "MEDIOCRITY";
-    }
-    if (music.currentTime / music.duration > 0.33 && music.currentTime / music.duration < 0.525) {
-        output.innerText = "JUST";
-    }
-    if (music.currentTime / music.duration > 0.525 && music.currentTime / music.duration < 0.56) {
-        output.innerText = "ONE";
-    }
-    if (music.currentTime / music.duration > 0.56 && music.currentTime / music.duration < 0.59) {
-        output.innerText = "LICK";
-    }
-    if (music.currentTime / music.duration > 0.59 && music.currentTime / music.duration < 0.62) {
-        output.innerText = "AND";
-    }
-    if (music.currentTime / music.duration > 0.62 && music.currentTime / music.duration < 0.65) {
-        output.innerText = "YOU'LL";
-    }
-    if (music.currentTime / music.duration > 0.65 && music.currentTime / music.duration < 0.68) {
-        output.innerText = "SUCK";
-    }
-    if (music.currentTime / music.duration > 0.68 && music.currentTime / music.duration < 0.71) {
-        output.innerText = "FOREVER";
-    }
-    if (music.currentTime / music.duration > 0.71 && music.currentTime / music.duration < 0.74) {
-        output.innerText = "SUCK";
-    }
-    if (music.currentTime / music.duration > 0.74 && music.currentTime / music.duration < 0.84) {
-        output.innerText = "FOREVER";
-    }
-    if (music.currentTime / music.duration > 0.84 && music.currentTime / music.duration < 1) {
-        output.innerText = "SUCK";
+   
     }
     requestAnimationFrame(update);
 }
